cd512c3b32ac9dcb8083fe5456636c46a4d0f52a
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-06-24 18:32:39 -0700

    python

---
+++ python/counting.py
@@ -0,0 +1,11 @@
+input("Start?")
+
+
+def count_to_infinity():
+    i = 0
+    while i < 1_000_000:
+        i += 1
+    print(i)
+
+
+count_to_infinity()
--- python/subprocesses.py
+++ python/subprocesses.py
@@ -1,4 +1,5 @@
-i = 0
-while i < 1_000_000_000:
-    i += 1
-print(i)
+import subprocess
+
+
+if __name__ == "__main__":
+    subprocess.run(["python3", "counting.py"])
