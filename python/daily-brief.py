4114b6dc026e2afc54be6c308b9eaabc21a4fda6
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-06-29 10:58:02 -0700

    WTF is this.

--- python/daily-brief.py
+++ python/daily-brief.py
@@ -2,10 +2,20 @@
 # Open daily brief of websites in browser
 import webbrowser
 import argparse
+from enum import Enum
 
 
 class DailyBrief:
-    BRIEF_LIST = [
+    """Opens a daily brief of cool stuff in the browser."""
+
+    # List of urls to open
+    class ContentType(Enum):
+        DAILY = 1
+        NEWS = 2
+        RESEARCH = 3
+        BLOGS = 4
+
+    URLS_DAILY = [
         "https://companiesmarketcap.com/assets-by-market-cap/",
         "https://www.coingecko.com/",
         "https://github.com/trending",
@@ -16,17 +26,31 @@ class DailyBrief:
         "https://twitter.com/CoinbaseAssets",
         "https://ultrasound.money/",
         "https://clientdiversity.org/#distribution",
+        "https://defillama.com/",
     ]
 
-    BRIEF_NEWS = [
+    URLS_NEWS = [
         "https://rekt.news",
         "https://weekinethereumnews.com/",
+        "https://www.xda-developers.com/",
+        "https://www.visualcapitalist.com",
+        "https://physicsworld.com",
+        "https://arstechnica.com/",
+        "https://www.investopedia.com",
     ]
 
-    RESEARCH_NEWS = [
-        "https://weekinethereumnews.com/",
+    URLS_RESEARCH = [
         "https://forum.soliditylang.org/",
-        "https://ethresear.ch/" "https://collective.flashbots.net/latest",
+        "https://ethresear.ch/",
+        "https://collective.flashbots.net/latest",
+    ]
+
+    URLS_BLOGS = [
+        "https://dbless.dev",
+        "https://cmichel.io",
+        "https://simonelnahas.com/",
+        "https://www.saianeesh.com/",
+        "https://hackernoon.com/exit-liquidity",
     ]
 
     def parse_args(self):
@@ -37,7 +61,7 @@ class DailyBrief:
         parser.add_argument(
             "-n",
             "--news",
-            type=bool,
+            action="store_true",
             help="Opens news sites.",
         )
 
@@ -49,19 +73,53 @@ class DailyBrief:
             help="Show program's version number and exit.",
         )
 
+        parser.add_argument(
+            "--research",
+            "-r",
+            action="store_true",
+            help="Opens research sites.",
+        )
+
+        parser.add_argument(
+            "--blogs",
+            "-b",
+            action="store_true",
+            help="Opens blogs.",
+        )
+
         args = parser.parse_args()
 
         return args
 
     def __init__(self):
         args = self.parse_args()
-        self.open_news = args.news
+        if args.news:
+            self.content = self.ContentType.NEWS
+        elif args.research:
+            self.content = self.ContentType.RESEARCH
+        elif args.blogs:
+            self.content = self.ContentType.BLOGS
+        else:
+            self.content = self.ContentType.DAILY
 
-    def open_brief(self):
-        for url in self.BRIEF_LIST:
+    def open_list(self, list):
+        """Opens a list of urls in the browser."""
+        for url in list:
             webbrowser.open_new_tab(url)
+
+    def run(self):
+        match self.content:
+            case self.ContentType.NEWS:
+                self.open_list(self.URLS_NEWS)
+            case self.ContentType.RESEARCH:
+                self.open_list(self.URLS_RESEARCH)
+            case self.ContentType.BLOGS:
+                self.open_list(self.URLS_BLOGS)
+            case self.ContentType.DAILY:
+                self.open_list(self.URLS_DAILY)
+            case _:
+                print("Invalid content type")
 
 
 if __name__ == "__main__":
-    daily_brief = DailyBrief()
-    daily_brief.open_brief()
+    DailyBrief().run()
