e8218285ce95646074bdfaa4b050cea3f11bad3b
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-09-16 20:27:57 -0700

    go

--- pl/go/counting_high.go
+++ pl/go/counting_high.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var counter int64
-	for counter = 0; counter < 100_000_000_000; counter++ {
+	for counter = 0; counter < 10_000_000_000; counter++ {
 	}
 	fmt.Println(counter)
 }
