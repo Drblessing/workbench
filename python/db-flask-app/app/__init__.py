588d2c2ae7bc52f6dedb7301f768b12d0f1f5b57
Danny Blessing <drblessing@gmail.com>
2024-01-25 23:36:42 -0800

    db-flask-app

--- python/db-flask-app/app/__init__.py
+++ python/db-flask-app/app/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 db = SQLAlchemy(app)
 
 from app import routes
+
+with app.app_context():
+    db.create_all()
--- python/db-flask-app/app/models.py
+++ python/db-flask-app/app/models.py
@@ -1,9 +1,13 @@
 from app import db
+import uuid
+
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    task = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100))
+    priority = db.Column(db.String(50))
     completed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return f'<Task {self.content}>'
+        return f"<Task {self.task}>"
