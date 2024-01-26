bdeb00d49b5b5f2f3aa33c08d8157eea8b9d6c07
Danny Blessing <drblessing@gmail.com>
2024-01-25 21:36:40 -0800

    simple flask todo

--- python/simple-flask-todo/app.py
+++ python/simple-flask-todo/app.py
@@ -1,16 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
+import re
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class Task:
+    """A simple dataclass to represent a task"""
+
+    task: str
+    priority: str = "low"
+    email: str | None = None
+    done: bool = False
+
 
 app = Flask(__name__)
 
 # This will be our in-memory tasks list
-tasks = []
+tasks: list[Task] = []
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         task_content = request.form["content"]
-        tasks.append(task_content)
+        task_priority = request.form.get("priority", "low")
+        task_email = request.form.get("email")
+
+        task = Task(
+            task=task_content,
+            priority=task_priority,
+            email=task_email,
+            done=False,
+        )
+
+        tasks.append(task)
         return redirect(url_for("index"))
     else:
         return render_template("index.html", tasks=tasks)
