68fd6c5ec62ba4d393ed4e275232dcf3b5633e61
Danny Blessing <drblessing@gmail.com>
2024-01-27 19:16:55 -0800

    main

--- python/package/package1/module1.py
+++ python/package/package1/module1.py
@@ -1,2 +1,8 @@
+# import get from the utils package, and use it to get the logger.
+from utils.log_manager import get_logger
+
+
 def function1():
+    logger = get_logger()
+    logger.info("function1 called")
     return "Hello from module1 in package1"
