dce00a1b379c453d2fb57e0db7fdf475d12927e9
Danny Blessing <drblessing@gmail.com>
2024-01-31 18:59:51 -0800

    random stuff

---
+++ python/leetcode/Leetcode 75/Two Pointers/1679. Max Number of K-Sum Pairs.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+
+class Solution:
+    def maxOperations(self, nums: list[int], k: int) -> int:
+        """Calculate the maximum number of pairs that sum to k."""
+
+        # Iterate through
