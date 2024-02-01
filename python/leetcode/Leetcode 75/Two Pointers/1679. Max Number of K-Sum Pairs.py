23c758796da16b1aa629f180dc71d76383b23869
Danny Blessing <drblessing@gmail.com>
2024-01-31 19:21:49 -0800

    max numbers

--- python/leetcode/Leetcode 75/Two Pointers/1679. Max Number of K-Sum Pairs.py
+++ python/leetcode/Leetcode 75/Two Pointers/1679. Max Number of K-Sum Pairs.py
@@ -5,4 +5,26 @@ class Solution:
     def maxOperations(self, nums: list[int], k: int) -> int:
         """Calculate the maximum number of pairs that sum to k."""
 
-        # Iterate through
+        # Count the frequency of each number
+        counter = Counter(nums)
+
+        # Initialize the number of pairs
+        pairs = 0
+        # Iterate through the numbers, checking for k - num
+        for num in counter:
+            # If the number is greater than k, skip it
+            if num > k:
+                continue
+
+            # If the number is half of k, add half the frequency to pairs
+            if num * 2 == k:
+                pairs += counter[num] // 2
+            # Otherwise, add the minimum of the frequency of num and k - num, if it's less than 1/2 of k
+            # to avoid duplicates with changing the dictionary while iterating,
+            # clever
+
+            elif num < k / 2 and k - num in counter:
+                pairs += min(counter[num], counter[k - num])
+                # Clear the frequency of num and k - num
+
+        return pairs
