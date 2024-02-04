b2accfae01ce61a1b8687813e32b648ccff6fd25
Danny Blessing <drblessing@gmail.com>
2024-02-03 21:03:27 -0800

    rows and columns

--- python/leetcode/Leetcode 75/hashmap-set/2352. Equal Row and Column Pairs.py
+++ python/leetcode/Leetcode 75/hashmap-set/2352. Equal Row and Column Pairs.py
@@ -4,17 +4,10 @@ from collections import Counter
 class Solution:
     def equalPairs(self, grid: list[list[int]]) -> int:
         # Count the occurrences of each row pattern
-        row_patterns = Counter(tuple(row) for row in grid)
+        row_c = Counter(tuple(row) for row in grid)
 
         # Count the occurrences of each column pattern
-        column_patterns = Counter(
-            tuple(grid[row][col] for row in range(len(grid)))
-            for col in range(len(grid[0]))
-        )
+        col_c = Counter(zip(*grid))
 
         # Count congruent pairs by matching row and column patterns
-        congruent_pairs = sum(
-            row_patterns[row] * column_patterns.get(row, 0) for row in row_patterns
-        )
-
-        return congruent_pairs
+        return sum([row_c[row] * col_c[row] for row in row_c])
