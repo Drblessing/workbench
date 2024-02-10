11fa89308924a0554626b88963e65624d83b3c3c
Danny Blessing <drblessing@gmail.com>
2024-02-10 08:16:54 -0800

    scripting

--- python/scripting.py
+++ python/scripting.py
@@ -1,4 +1,42 @@
-import os
+import subprocess
 
-# Running a simple bash command
-os.system('echo "Hello, simpler world!"')
+
+class BashRunner:
+    def __init__(self, script):
+        self.script = script
+
+    def run(self):
+        """
+        Executes the stored Bash script and returns the output and error.
+        """
+        # Summoning the process
+        process = subprocess.Popen(
+            ["bash"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        # Whispering the incantations to the void
+        stdout, stderr = process.communicate(self.script)
+
+        # The spirits respond
+        return stdout, stderr
+
+
+# Your Bash script, a spell woven in strings
+bash_script = """
+echo "Hello from the other side"
+ls -l /nonexistentdirectory 2>&1
+"""
+
+# The invocation
+wizard = BashRunner(bash_script)
+output, errors = wizard.run()
+
+print("Output:", output)
+if errors:
+    print("Errors:", errors)
+else:
+    print("No errors, the spell was perfect!")
