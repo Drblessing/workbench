7849b0bf2b281d0f1b30e86bc6ee9b7634bf4849
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2024-02-13 20:54:30 -0800

    embedding flask videos

---
+++ python/embedding_videos/main.py
@@ -0,0 +1,16 @@
+"""Very simple flask template for 
+embedding videos in a webpage.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
